074e3b6a530871cff8be1b75d32a9bccac04f2d1
JHsparrow <aodrkvotm123@naver.com>
2023-02-19 21:07:02 +0900

    콘텐츠

--- src/main/java/ChamStudy/Controller/AdminClassController.java
+++ src/main/java/ChamStudy/Controller/AdminClassController.java
@@ -5,23 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="adminClass")
+@RequestMapping(value="adminContent")
 public class AdminClassController {
 	
-	@GetMapping(value = "/generation") //기수제 목록
+	@GetMapping(value = "/category") //카테고리 관리
 	public String generation() {
-		return "AdminClass/list";
+		return "AdminContent/cateList";
 	}
 	
-	@GetMapping(value = "/onlineList") //실시간강의 목록
-	public String onlineList() {
-		return "AdminForm/AdminClass/onlineList";
-	}
 	
-	@GetMapping(value = "/onlineDetail") //강의 상세페이지
-	public String onlineDetail() {
-		return "AdminForm/AdminClass/onlineDetail";
-	}
 	
 	
 }
