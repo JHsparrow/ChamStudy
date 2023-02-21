a57351d7955c77483aba99b4d869c2ea6751c21b
김명현 <52224909+myeonghyeonKim0922@users.noreply.github.com>
2023-02-21 11:40:31 +0900

    up

--- src/main/java/ChamStudy/Controller/AdminCommController.java
+++ src/main/java/ChamStudy/Controller/AdminCommController.java
@@ -1,9 +1,11 @@
 package ChamStudy.Controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ChamStudy.Dto.AdminMainCommDto;
 import ChamStudy.Service.AdminCommService;
@@ -53,13 +56,16 @@ public class AdminCommController { // 관리자 커뮤니티 게시판
 	
 	//게시판 상세 페이지
 	@GetMapping(value = "/comm/dtl/{boardId}")
-	public String commDtl(@PathVariable("boardId") Long boardId, Model model) {
+	public String commDtl(@PathVariable("boardId") Long boardId, Model model, HttpServletRequest request) {
 		try {
 			AdminMainCommDto adminMainCommDto = adminCommService.getAdminCommDtl(boardId);
 			model.addAttribute("comm",adminMainCommDto);
 		}catch(EntityNotFoundException e) {
 			model.addAttribute("errorMessage","존재하지 않는 게시물입니다.");
 		}
+		String referer = request.getHeader("Referer");
+			request.getSession().setAttribute("redirectURI", referer);
+			model.addAttribute("referer",referer);
 		return "AdminForm/AdminComm/comm-Dtl-Form";
 	}
 
@@ -72,11 +78,9 @@ public class AdminCommController { // 관리자 커뮤니티 게시판
 		return "redirect:" + referer;
 	}
 	
-	@GetMapping(value = "/comm/back")
-	public String rateHandler(HttpServletRequest request) {
-	    //your controller code
-	    String referer = request.getHeader("Referer");
-	    return "redirect:"+ referer;
+	@RequestMapping(value = "/comm/back" ,method = RequestMethod.GET)
+	public void rateHandler(@RequestParam("referer") String url,HttpServletResponse httpServletResponse) throws IOException {
+	     httpServletResponse.sendRedirect(url);
 	}
 	
 	
