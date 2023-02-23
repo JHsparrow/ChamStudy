b0f093fd858695875301a15d35a09882f578f921
KYS1126 <kimyongsu1226@naver.com>
2023-02-23 14:43:44 +0900

    up

--- src/main/java/ChamStudy/Repository/UserRepository.java
+++ src/main/java/ChamStudy/Repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import ChamStudy.Entity.UserInfo;
+import ChamStudy.Impl.UserRepositoryCustom;
 
 public interface UserRepository extends JpaRepository<UserInfo, Long>,
 QuerydslPredicateExecutor<UserInfo>, UserRepositoryCustom{
