a6cb03f3b1917f376fd9d084551a4550dc4fb373
JHsparrow <aodrkvotm123@naver.com>
2023-03-03 01:08:41 +0900

    차트 자동 분류

--- src/main/java/ChamStudy/Repository/ApplyRepository.java
+++ src/main/java/ChamStudy/Repository/ApplyRepository.java
@@ -1,10 +1,12 @@
 package ChamStudy.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import ChamStudy.Dto.ChartInterface;
 import ChamStudy.Entity.ApplyList;
 import ChamStudy.Entity.Category;
 import ChamStudy.Entity.ContentVideo;
@@ -14,5 +16,16 @@ public interface ApplyRepository extends JpaRepository<ApplyList, Long> {
 	Long countMain(String mainName);
 	
 	@Query(value= "select count(*) from apply_list a inner join class_info b on a.class_id = b.class_id inner join content_info c on c.content_id = b.content_id inner join category d on d.category_id = c.category_id inner join sub_category e on d.category_id = e.category_id and c.sub_category_id = e.sub_category_id where d.name = ?1 and e.name = ?2",nativeQuery = true)
-	Long countMainSub(String mainName, String subName); 
+	Long countMainSub(String mainName, String subName);
+	
+	@Query(value="select d.name as name, count(*) as count from apply_list a inner join class_info b on a.class_id = b.class_id inner join content_info c on c.content_id = b.content_id inner join category d on d.category_id = c.category_id group by a.class_id", nativeQuery = true)
+	List<ChartInterface> countChartMain();
+	
+	@Query(value="select e.name as name, count(*) as count from apply_list a inner join class_info b on a.class_id = b.class_id inner join content_info c on c.content_id = b.content_id inner join category d on d.category_id = c.category_id inner join sub_category e on d.category_id = e.category_id and c.sub_category_id = e.sub_category_id where d.name='IT' group by a.class_id order by e.name", nativeQuery = true)
+	List<ChartInterface> countChartSubIt();
+	@Query(value="select e.name as name, count(*) as count from apply_list a inner join class_info b on a.class_id = b.class_id inner join content_info c on c.content_id = b.content_id inner join category d on d.category_id = c.category_id inner join sub_category e on d.category_id = e.category_id and c.sub_category_id = e.sub_category_id where d.name='자격증' group by a.class_id order by e.name", nativeQuery = true)
+	List<ChartInterface> countChartSubCertificate();
+	@Query(value="select e.name as name, count(*) as count from apply_list a inner join class_info b on a.class_id = b.class_id inner join content_info c on c.content_id = b.content_id inner join category d on d.category_id = c.category_id inner join sub_category e on d.category_id = e.category_id and c.sub_category_id = e.sub_category_id where d.name='어학' group by a.class_id order by e.name", nativeQuery = true)
+	List<ChartInterface> countChartSubLanguage();
+	
 }
--- src/main/java/ChamStudy/Service/AdminMainService.java
+++ src/main/java/ChamStudy/Service/AdminMainService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import ChamStudy.Dto.CategoryDto;
 import ChamStudy.Dto.CategoryInterface;
+import ChamStudy.Dto.ChartInterface;
 import ChamStudy.Dto.EducationInfoDto;
 import ChamStudy.Dto.EducationInfoInterface;
 import ChamStudy.Dto.MainCategoryDto;
@@ -69,22 +70,19 @@ public class AdminMainService {
 	public Long countApplyList() {
 		return applyRepository.count();
 	}
+	public List<ChartInterface> countChartMain(){
+		return applyRepository.countChartMain();
+	}
 	
-	public Long countApplyListMainIt() { return applyRepository.countMain("IT");}
-	public Long countApplyListMainLi() { return applyRepository.countMain("자격증");}
-	public Long countApplyListMainLa() { return applyRepository.countMain("어학");}
-	
-	public Long countApplyListMainItSubJ() { return applyRepository.countMainSub("IT","자바");}
-	public Long countApplyListMainItSubP() { return applyRepository.countMainSub("IT","파이썬");}
-	public Long countApplyListMainItSubEt() { return applyRepository.countMainSub("IT","기타");}
-	
-	public Long countApplyListMainLiSubE() { return applyRepository.countMainSub("자격증","엑셀");}
-	public Long countApplyListMainLiSubP() { return applyRepository.countMainSub("자격증","공무원");}
-	public Long countApplyListMainLiSubEt() { return applyRepository.countMainSub("자격증","기타");}
-	
-	public Long countApplyListMainLaSubC() { return applyRepository.countMainSub("어학","중국어");}
-	public Long countApplyListMainLaSubE() { return applyRepository.countMainSub("어학","영어");}
-	public Long countApplyListMainLaSubEt() { return applyRepository.countMainSub("어학","기타");}
+	public List<ChartInterface> countChartSubIt(){
+		return applyRepository.countChartSubIt();
+	}
+	public List<ChartInterface> countChartSubCertificate(){
+		return applyRepository.countChartSubCertificate();
+	}
+	public List<ChartInterface> countChartSubLanguage(){
+		return applyRepository.countChartSubLanguage();
+	}
 	
 	public List<EducationInfoInterface> educationInfo(){
 		return classInfoRepository.educationInfo();
