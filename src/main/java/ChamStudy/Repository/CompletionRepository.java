5d3448e8bb082be93bff0559d373a4fd9152fcd2
JHsparrow <aodrkvotm123@naver.com>
2023-03-03 18:42:41 +0900

    수료 완료

--- src/main/java/ChamStudy/Repository/CompletionRepository.java
+++ src/main/java/ChamStudy/Repository/CompletionRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 
 import ChamStudy.Entity.Completion;
 
+
 public interface CompletionRepository extends JpaRepository<Completion, Long> {
 	
-	
+	@Query(value="select * from completion where result_id = ?1", nativeQuery = true)
+	Completion getCompletion(Long resultId); 
 
 }
