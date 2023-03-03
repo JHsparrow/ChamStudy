0c6997704cd3a4c434a003fe77e09feabeedee33
eunmiyo <good3103@gmail.com>
2023-03-03 17:43:07 +0900

    test

---
+++ src/main/java/ChamStudy/Dto/Class_reviewDto.java
@@ -0,0 +1,17 @@
+package ChamStudy.Dto;
+
+import ChamStudy.Entity.ClassInfo;
+import ChamStudy.Entity.UserInfo;
+
+public interface Class_reviewDto {
+	
+	Long id;
+	
+	String description;
+	
+	Integer starPoint;
+	
+	UserInfo userInfo;
+	
+	ClassInfo classInfo;
+}
