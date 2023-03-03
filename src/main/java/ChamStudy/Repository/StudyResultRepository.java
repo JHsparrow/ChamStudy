dfc5946cb466420b2cc9353d3b717c004c87aa69
JHsparrow <aodrkvotm123@naver.com>
2023-03-03 16:18:35 +0900

    study_result 수정완료

---
+++ src/main/java/ChamStudy/Repository/StudyResultRepository.java
@@ -0,0 +1,13 @@
+package ChamStudy.Repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import ChamStudy.Entity.StudyResult;
+
+public interface StudyResultRepository extends JpaRepository<StudyResult, Long> {
+	
+	@Query(value="select result_id from study_result where apply_id = ?1", nativeQuery = true)
+	Long getResultId(Long applyId);
+}
--- src/main/java/ChamStudy/Service/ContentVideoService.java
+++ src/main/java/ChamStudy/Service/ContentVideoService.java
@@ -8,13 +8,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ChamStudy.Dto.VideoDto;
+import ChamStudy.Entity.ApplyList;
 import ChamStudy.Entity.ContentInfo;
 import ChamStudy.Entity.ContentVideo;
 import ChamStudy.Entity.StudyHistory;
+import ChamStudy.Entity.StudyResult;
 import ChamStudy.Entity.UserInfo;
+import ChamStudy.Repository.ApplyListRepository;
 import ChamStudy.Repository.OnContentRepository;
 import ChamStudy.Repository.OnContentVideoRepository;
 import ChamStudy.Repository.StudyHistortRepository;
+import ChamStudy.Repository.StudyResultRepository;
 import ChamStudy.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -27,6 +31,9 @@ public class ContentVideoService {
 	private final StudyHistortRepository studyHistortRepository;
 	private final OnContentRepository onContentRepository;
 	private final UserRepository userRepository;
+	private final ApplyListRepository applyListRepository;
+	private final StudyResultRepository studyResultRepository;
+	
 	
 	public void insertVideoData(ContentInfo contentId, int count) {
         for (int i = 1; i <= count; i++) {
@@ -55,19 +62,39 @@ public class ContentVideoService {
 		ContentInfo getContentId = onContentRepository.getContentId(contentId);
 		Long history_id = studyHistortRepository.getVideoId(videoId.getId());
 		UserInfo userId = userRepository.getUserId(email);
+		ApplyList applyList = applyListRepository.findByUserId(userId.getId());
+		
 		StudyHistory studyHistory = new StudyHistory();
 		if(history_id == null) {
 			studyHistory.setVideoId(videoId);
 			studyHistory.setContentId(getContentId);
-			studyHistory.setUserId(userId);
+			studyHistory.setApplyId(applyList);
 		} else {
 			studyHistory.setVideoId(videoId);
 			studyHistory.setContentId(getContentId);
 			studyHistory.setId(history_id);
-			studyHistory.setUserId(userId);
 		}
 		studyHistortRepository.save(studyHistory);
 	}
+	
+	
+	public void createStudyResult(String email,Long contentId) {
+		UserInfo userId = userRepository.getUserId(email);
+		ApplyList applyId = applyListRepository.findByUserId(userId.getId());
+		Long progress = studyHistortRepository.getProgress(applyId.getId(),contentId);
+		Long validResult = studyResultRepository.getResultId(applyId.getId());
+		
+		StudyResult studyResult = new StudyResult();
+		
+		if(validResult != null) {
+			studyResult.setId(validResult);
+		}
+		studyResult.setApplyId(applyId);
+		studyResult.setProgress(progress);
+		studyResultRepository.save(studyResult);
+	}
+	
+	
 	
 	public Long getProcess(Long contentId) {
 		return studyHistortRepository.getCountVideoId(contentId);
