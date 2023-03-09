4260b6033e5332091e0cb800c3a50a9f8f23ece1
JHsparrow <aodrkvotm123@naver.com>
2023-03-09 15:10:36 +0900

    마무리작업

--- src/main/java/ChamStudy/ChamStudyApplication.java
+++ src/main/java/ChamStudy/ChamStudyApplication.java
@@ -2,12 +2,22 @@ package ChamStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChamStudyApplication {
+public class ChamStudyApplication extends SpringBootServletInitializer{
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ChamStudyApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ChamStudyApplication.class);
+
+	}
+	
+	
 }
